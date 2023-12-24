8c60a1508415c297d5390b54a00681eb6c3a4d11
cdlrios <carlos.delosriosmouvet2@gmail.com>
2023-12-24 18:44:01 +0100

    nochebuena

--- fastapi/server.py
+++ fastapi/server.py
@@ -11,56 +11,48 @@ class BaseModel(PydanticBaseModel):
         arbitrary_types_allowed = True
 
 class Partido(BaseModel):
-    Date:str
-    HomeTeam:str
-    AwayTeam:str
-    FTHG:int
-    FTAG:int
-    FTR:str
-    HTHG:int
-    HTAG:int
-    HTR:str
-    HS:int
-    AS:int
-    HST:int
-    AST:int
-    HF:int
-    AF:int
-    HC:int
-    AC:int
-    HY:int
-    AY:int
-    HR:int
-    AR:int
-    B365H:float
-    B365D:float
-    B365A:float
-    BWH:float
-    BWD:float
-    BWA:float
-    IWH:float
-    IWD:float
-    IWA:float
-    LBH:float
-    LBD:float
-    LBA:float
-    PSH:float
-    PSD:float
-    PSA:float
-    WHH:float
-    WHD:float
-    WHA:float
-    VCH:float
-    VCD:float
-    VCA:float
-    Temporada:int
-
-class ListaPartidos(BaseModel):
-    partidos: List[Partido]  # Add the type annotation
+    Index: int
+    Date: str
+    HomeTeam: str
+    AwayTeam: str
+    FTHG: int
+    FTAG: int
+    FTR: str
+    HTHG: int
+    HTAG: int
+    HTR: str
+    HS: int
+    AS: int
+    HST: int
+    AST: int
+    HF: int
+    AF: int
+    HC: int
+    AC: int
+    HY: int
+    AY: int
+    HR: int
+    AR: int
+    B365H: float
+    B365D: float
+    B365A: float
+    BWH: float
+    BWD: float
+    BWA: float
+    IWH: float
+    IWD: float
+    IWA: float
+    WHH: float
+    WHD: float
+    WHA: float
+    VCH: float
+    VCD: float
+    VCA: float
+    Temporada: int
 
 app = FastAPI(
     title="Servidor de datos",
-    description="""Servimos datos de la liga""",
+    description="Servimos datos de la liga",
     version="0.1.0",
 )
 
@@ -68,5 +60,4 @@ app = FastAPI(
 def retrieve_data():
     todosmisdatos = pd.read_csv('fastapi/dataliga.csv')
     todosmisdatosdict = todosmisdatos.to_dict(orient='records')
-    listado = ListaPartidos(partidos=todosmisdatosdict)  # Provide data during instance creation
-    return listado
+    return todosmisdatosdict
--- streamlit/main.py
+++ streamlit/main.py
@@ -1,91 +1,44 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import altair as alt
-import time
+import pandas as pd
 import requests
 
-response = requests.get("http://fastapi:8000//retrieve_data/")
-data = response.json()
-partidos = pd.DataFrame(data)
+@st.cache
+def load_data(url: str):
+    r = requests.get(url)
+    mijson = r.json()
+    listado = mijson['partidos']
+    partidos = pd.DataFrame.from_records(listado)
+    return partidos
 
-#Creo un método para obtener las columnas con datos vacios de los dataframes.
+# URL del archivo CSV, ajusta según sea necesario
+url_csv = 'http://localhost:8000/retrieve_data/'
+partidos = load_data(url_csv)
+
+# Métodos para comprobar y quitar valores vacíos, según sea necesario
 def compruebavacios(dataframe):
-  valores_vacios = []
-  for column_name in dataframe.columns:
-    if dataframe[column_name].isna().any():
-      valores_vacios.append(column_name)
+    valores_vacios = [column_name for column_name in dataframe.columns if dataframe[column_name].isna().any()]
+    if not valores_vacios:
+        print("No hay valores vacíos en este DataFrame")
     else:
-      continue
-  if len(valores_vacios)==0:
-    print("No hay valores vacios en este dataframe")
-  else:
-    print("Las columnas con valores vacios son: ")
-    for i in valores_vacios:
-      print(i)
+        print("Las columnas con valores vacíos son:")
+        for i in valores_vacios:
+            print(i)
 
-#creo un método para borrar aquellas columnas que todos sus valores estén vacios
 def quitavacios(dataframe):
-  for column_name in dataframe.columns:
-    if dataframe[column_name].isna().all():
-      dataframe.drop(column_name, axis=1, inplace=True)
-    else:
-      continue
+    dataframe.dropna(axis=1, how='all', inplace=True)
 
-
-#printeo los nombres de los equipos en el dataframe
-print(partidos["HomeTeam"].unique())
-print(len(partidos))
-
-
-#Para una mejor comprensión de los datos, sustituimos los nombres de los equipos por sus abreviaturas
-#equipos_1617 = {"Nombre": equipos, "Abreviatura": ["ALA", "ATH", "ATM", "FCB", "BET", "CEL", "EIB", "ESP", "GRA", "DEP", "LPA", "LEG", "MAL", "OSA", "RMA", "SEV", "RSO", "SPO", "VAL", "VILL"]}
-#df_temporada_1617 = pd.DataFrame(equipos_1617)
-
-#Sustituyo los nombres de los equipos por sus abreviaturas en el dataframe original
-#partidos["HomeTeam"] = partidos["HomeTeam"].map(df_temporada_1617.set_index("Nombre")["Abreviatura"])
-#partidos["AwayTeam"] = partidos["AwayTeam"].map(df_temporada_1617.set_index("Nombre")["Abreviatura"])
-
-#print(partidos)
-
-# Datos de ejemplo para la temporada 16/17 con nombres y abreviaturas
-#datos_temporada_1617 = {"Nombre": ["Real Madrid", "Barcelona", "Atlético de Madrid", "Sevilla", "Villarreal", "Real Sociedad", "Athletic Bilbao", "Eibar", "Espanyol", "Alavés", "Málaga", "Valencia", "Celta de Vigo", "Las Palmas", "Real Betis", "La Coruna", "Leganés", "Sporting Gijón", "Osasuna", "Granada"], "Abreviatura": ["RMA", "BAR", "ATM", "SEV", "VIL", "RSO", "ATH", "EIB", "ESP", "ALA", "MAL", "VAL", "CEL", "LPA", "RBB", "DEP", "LEG", "SPO", "OSA", "GRA"]}
-
-
-#df.fillna(0, inplace=True)  # Reemplaza los valores faltantes con 0 (u otro valor)
-#df = df[df['columna'] > 0]  # Mantiene solo las filas donde 'columna' es mayor a 0
-#df.rename(columns={'viejo_nombre': 'nuevo_nombre'}, inplace=True)
-#df.drop(['columna_a_eliminar'], axis=1, inplace=True)
-#df = df[(df['columna'] > limite_inferior) & (df['columna'] < limite_superior)]
-#df['columna'] = (df['columna'] - df['columna'].mean()) / df['columna'].std()
-#df.to_csv('tu_archivo_limpio.csv', index=False)
-
-#Creo un método para obtener las columnas con datos vacios de los dataframes.
-def compruebavacios(dataframe):
-  valores_vacios = []
-  for column_name in dataframe.columns:
-    if dataframe[column_name].isna().any():
-      valores_vacios.append(column_name)
-    else:
-      continue
-  if len(valores_vacios)==0:
-    print("No hay valores vacios en este dataframe")
-  else:
-    print("Las columnas con valores vacios son: ")
-    for i in valores_vacios:
-      print(i)
-
+# Comprueba y quita valores vacíos
 compruebavacios(partidos)
 quitavacios(partidos)
+
+# Título de la aplicación Streamlit
 st.title('Campeonato Nacional de Liga de Primera División')
 
+# Selector de equipo
 equipo_seleccionado = st.selectbox('Selecciona un equipo:', partidos['HomeTeam'].unique())
 
+# Filtra los partidos según el equipo seleccionado
 partidos_filtrados = partidos[partidos['HomeTeam'] == equipo_seleccionado]
 
+# Muestra el DataFrame filtrado
 st.dataframe(partidos_filtrados)
-
-
-
-
-
